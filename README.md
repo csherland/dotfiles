7ea9cff75917f954e17aeeeb8a77dba541721f6c
Christian <christiansherland@gmail.com>
2014-06-27 09:40:26 -0400

    basic working install script?

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 Dotfiles
 ========
-
 A compliation of my OSX dotfiles including an installation script.
-
-Contents
---------
 
 Usage
 -----
+To install all components, as well as a small selection of required packages run
+
+  git clone http://github.com/csherland/dotfiles ~/.dotfiles && ~/.dotfiles/install.sh
+
+The script may require confirmation for a few installations, but otherwise, thats
+all that is required for installation.
+
+Contents
+--------
 
 License
 -------
--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-
+#
 # Christian Sherland
 # 6/23/14
 #
 # install.sh
-#   Setup script for my dotfiles on an OSX machine. 
-#
+#   Setup script for my dotfiles on an OSX machine.
 #
 
+# Homebrew!
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
+# Make sure you have everything you need!
+brew install git
+brew install zsh
+
+# Oh-my-zsh and zshrc
+curl -L http://install.ohmyz.sh | sh
+sudo chsh -s $(whoami) $(which zsh)
+ln -s ~/.zshrc ~/.dotfiles/zshrc
+source ~/.zshrc
+
+# Global gitignore
+ln -s ~/.gitignore_global ~/.dotfiles/gitignore_global
+git config --global core.excludesfile '~/.gitignore'
+
+# vimrc
+mkdir ~/.vim
+ln -s ~/.vim/vimrc ~/.dotfiles/vimrc
+ln -s ~/.vimrc ~/.vim/vimrc
