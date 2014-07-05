6bcac7427c9c1ab37d79079d3465a6d3b7a4cd46
Christian <christiansherland@gmail.com>
2014-07-05 11:32:58 -0400

    added handbrake

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ brew cask install adium
 brew cask install atom
 brew cask install dropbox
 brew cask install google-chrome
+brew cask install handbrake
 brew cask install iterm2
 brew cask install spotify
 brew cask install steam
