2a50aff0ba8463e4192625d142bff52a693635b9
Christian <christiansherland@gmail.com>
2014-11-02 17:41:16 -0500

    Added specatcle window manager

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -31,6 +31,7 @@ brew cask install google-chrome
 brew cask install handbrake
 brew cask install iterm2
 brew cask install skype
+brew cask install spectacle
 brew cask install spotify
 brew cask install steam
 brew cask install vlc
