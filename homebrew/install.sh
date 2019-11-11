236663426567dc40ceaafc834c2cb68457e766aa
Christian Sherland <christiansherland@gmail.com>
2019-11-11 09:28:38 -0500

    more clean up

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -19,19 +19,18 @@ brew install git
 brew install htop
 brew install macvim
 brew install node
+brew install postgres
 brew install pt
 brew install zsh
 
 # Casks (non app-store apps)
 brew install caskroom/cask/brew-cask
+brew cask install adoptopenjdk8
 brew cask install alfred
 brew cask install amethyst
 brew cask install docker
-brew cask install hyper
 brew cask install iterm2
 brew cask install intellij-idea-ce
-brew cask install little-snitch
-brew cask install private-internet-access
 brew cask install postman
 brew cask install postbird
 brew cask install rocket
