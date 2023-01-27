82c89d60cfe3873b31e7f272292285b17680cf8d
Christian Sherland <christiansher@fb.com>
2023-01-27 09:07:08 -0500

    Clean up

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -18,26 +18,13 @@ brew install cmake
 brew install git
 brew install htop
 brew install macvim
-brew install node
-brew install postgres
 brew install pt
 brew install zsh
 
 # Casks (non app-store apps)
 brew install caskroom/cask/brew-cask
 #brew cask install adoptopenjdk8
-brew cask install alfred
 brew cask install amethyst
-brew cask install docker
-brew cask install iterm2
-#brew cask install intellij-idea-ce
-brew cask install postman
-brew cask install postbird
-brew cask install rocket
-brew cask install spotify
-brew cask install statusfy
-brew cask install visual-studio-code
-brew cask install zoomus
 
 # Inconsolata font, because it's awesome
 brew tap caskroom/fonts
--- npm/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# npm/install.sh
-#
-
-npm install -g eslint
-npm install -g prettier
-npm install -g n
