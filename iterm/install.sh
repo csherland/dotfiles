61b18df1cb0e211b43d07bfa6d33e499c9472eed
Christian Sherland <2327078+csherland@users.noreply.github.com>
2025-04-30 19:23:59 -0400

    remove iterm

--- iterm/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# iterm/install.sh
-#   Configures iterm2 with my custom preferences
-#
-
-# Configure location of iterm2 preferences
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder true
-defaults write com.googlecode.iterm2 PrefsCustomFolder "~/.dotfiles/iterm/"
