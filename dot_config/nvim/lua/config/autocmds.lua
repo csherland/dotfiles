79075df0304c549b60f5c3aa6cfed8b3c5e38c07
Christian Sherland <christiansherland@gmail.com>
2025-05-06 21:21:15 -0400

    Add chezmoi nvim plugin

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Apply chezmoi on changes
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+	callback = function(ev)
+		local bufnr = ev.buf
+		local edit_watch = function()
+			require("chezmoi.commands.__edit").watch(bufnr)
+		end
+		vim.schedule(edit_watch)
+	end,
+})
