8103732ea16c77d8103935a27998e6a3844f33f9
Christian Sherland <2327078+csherland@users.noreply.github.com>
2025-05-28 08:36:21 -0400

    semi working spaces in sketchybar

--- dot_config/sketchybar/plugins/executable_space.sh
+++ dot_config/sketchybar/plugins/executable_space.sh
@@ -2,13 +2,15 @@
 
 WIDTH="dynamic"
 SELECTED="false"
+HIGHLIGHT_BG="0xFF313244"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    WIDTH="0"
-    SELECTED="true"
+  WIDTH="0"
+  SELECTED="true"
+  HIGHLIGHT_BG="0xFF9399b2"
 fi
 
-sketchybar --animate tanh 10 --set $NAME \
-icon.highlight=$SELECTED \
-background.highlight=$SELECTED \
-label.width=$WIDTH
+sketchybar --animate tanh 5 --set $NAME \
+  icon.highlight=$SELECTED \
+  background.color=$HIGHLIGHT_BG \
+  label.width=$WIDTH
