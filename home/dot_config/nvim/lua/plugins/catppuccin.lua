35ce7d6e12ecbcd52c46c233e2d36d850cd544d2
csherland <2327078+csherland@users.noreply.github.com>
2025-06-01 08:41:49 -0400

    trying to figure out why cattpuccin isn't loading anymore

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,24 +1,18 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		-- you can do it like this with a config function
-		config = function()
-			require("catppuccin").setup({
-				-- configurations
-				flavour = "mocha",
-			})
-		end,
-		-- or just use opts table
-		opts = {
-			-- configurations
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
