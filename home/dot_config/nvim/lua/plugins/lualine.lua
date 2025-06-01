69e770e8bd12c27b3c896431fac239bc60129fd9
csherland <2327078+csherland@users.noreply.github.com>
2025-06-01 08:36:14 -0400

    rounded edges on lualine

---
+++ home/dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_y = {
+          { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          {
+            function()
+              return " " .. os.date("%R")
+            end,
+            separator = { right = "" },
+          },
+        },
+      },
+    },
+  },
+}
