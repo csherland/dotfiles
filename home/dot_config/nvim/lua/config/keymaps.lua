89b7e9cc9108d5995b25d48a4055cd0a9774d132
csherland <christiansherland@gmail.com>
2025-06-29 17:28:55 -0400

    stuff

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,7 @@ vim.keymap.set("v", "<C-Space>", "gc", { remap = true, desc = "Toggle comment (v
 vim.keymap.set("n", "<C-m>", ":bnext<CR>", { remap = true, desc = "Move to next buffer" })
 vim.keymap.set("n", "<C-b>", ":bprev<CR>", { remap = true, desc = "Move to prev buffer" })
 vim.keymap.set("n", "<C-x>", ":bp<CR>:bd #<CR>", { remap = true, desc = "Close buffer without saving" })
+
+-- copilot
+vim.keymap.set("n", "<leader>cpd", ":Copilot disable<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>cpe", ":Copilot enable<cr>", { silent = true, noremap = true })
