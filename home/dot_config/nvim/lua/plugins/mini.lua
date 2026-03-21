9d06082baefcfc0bd7c7aaa8be1b9a1e56204c9b
csherland <christiansherland@gmail.com>
2026-03-21 12:21:30 -0400

    add mini map excludes

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -22,7 +22,32 @@ return {
       })
       -- Dim minimap text to ~40% opacity against the background
       vim.api.nvim_set_hl(0, "MiniMapNormal", { link = "Comment" })
-      map.open()
+
+      -- Only open minimap for real file buffers
+      local function should_show_map()
+        local bt = vim.bo.buftype
+        local ft = vim.bo.filetype
+        local ignored_ft = { "snacks_dashboard", "dashboard", "alpha", "starter", "lazy", "neo-tree" }
+        if bt ~= "" then return false end
+        return not vim.tbl_contains(ignored_ft, ft)
+      end
+
+      -- Open on real files, close on non-file buffers
+      vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+          vim.schedule(function()
+            if should_show_map() then
+              map.open()
+            else
+              map.close()
+            end
+          end)
+        end,
+      })
+
+      if should_show_map() then
+        map.open()
+      end
     end,
     keys = {
       { "<leader>um", function() require("mini.map").toggle() end, desc = "Toggle minimap" },
