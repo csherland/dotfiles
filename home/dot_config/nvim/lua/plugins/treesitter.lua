6bb859212555bdabe7ea4d2fc13b64d9bbc9bf48
csherland <christiansherland@gmail.com>
2026-03-27 19:14:06 -0400

    misc updates

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -43,48 +43,46 @@ return {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		branch = "main",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = { query = "@function.outer", desc = "Select outer function" },
-							["if"] = { query = "@function.inner", desc = "Select inner function" },
-							["ac"] = { query = "@class.outer", desc = "Select outer class" },
-							["ic"] = { query = "@class.inner", desc = "Select inner class" },
-							["aa"] = { query = "@parameter.outer", desc = "Select outer argument" },
-							["ia"] = { query = "@parameter.inner", desc = "Select inner argument" },
-						},
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = { query = "@function.outer", desc = "Select outer function" },
+						["if"] = { query = "@function.inner", desc = "Select inner function" },
+						["ac"] = { query = "@class.outer", desc = "Select outer class" },
+						["ic"] = { query = "@class.inner", desc = "Select inner class" },
+						["aa"] = { query = "@parameter.outer", desc = "Select outer argument" },
+						["ia"] = { query = "@parameter.inner", desc = "Select inner argument" },
 					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]m"] = { query = "@function.outer", desc = "Next function start" },
-							["]]"] = { query = "@class.outer", desc = "Next class start" },
-						},
-						goto_next_end = {
-							["]M"] = { query = "@function.outer", desc = "Next function end" },
-							["]["] = { query = "@class.outer", desc = "Next class end" },
-						},
-						goto_previous_start = {
-							["[m"] = { query = "@function.outer", desc = "Previous function start" },
-							["[["] = { query = "@class.outer", desc = "Previous class start" },
-						},
-						goto_previous_end = {
-							["[M"] = { query = "@function.outer", desc = "Previous function end" },
-							["[]"] = { query = "@class.outer", desc = "Previous class end" },
-						},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = { query = "@function.outer", desc = "Next function start" },
+						["]]"] = { query = "@class.outer", desc = "Next class start" },
 					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next argument" },
-						},
-						swap_previous = {
-							["<leader>A"] = { query = "@parameter.inner", desc = "Swap with previous argument" },
-						},
+					goto_next_end = {
+						["]M"] = { query = "@function.outer", desc = "Next function end" },
+						["]["] = { query = "@class.outer", desc = "Next class end" },
+					},
+					goto_previous_start = {
+						["[m"] = { query = "@function.outer", desc = "Previous function start" },
+						["[["] = { query = "@class.outer", desc = "Previous class start" },
+					},
+					goto_previous_end = {
+						["[M"] = { query = "@function.outer", desc = "Previous function end" },
+						["[]"] = { query = "@class.outer", desc = "Previous class end" },
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next argument" },
+					},
+					swap_previous = {
+						["<leader>A"] = { query = "@parameter.inner", desc = "Swap with previous argument" },
 					},
 				},
 			})
